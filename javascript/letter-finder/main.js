ca71b4284b84e7e346c0bf5f8f59000e6c563cda
Marcel <marcel.mulders@hotmail.com>
2021-05-24 18:48:34 +0100

    commiting find-letter problem

---
+++ javascript/letter-finder/main.js
@@ -0,0 +1,32 @@
+/* Please find below the base code required for the problem */
+
+/*
+    PROBLEM:
+    **STRING LIST BELOW**
+    TASK 1: Write a function that takes a list of strings and returns the first that has the letter 'o' (pronounced "oh").
+    TASK 2: Return the first word that has two occurrences of 'o' in it.
+    TASK 3: 
+        Take min_z_count as a second parameter in your function and return the first word in the list that has 
+        at least that many 'o'’s OR null if there are no words in the list that contain that many instances of the letter.
+        You can either set min_z_count manually OR use a random number generator to set the value.
+
+    PRESENTATION:
+    - Please write clear, concise code that is appropriately commented.
+    - You may output everything using "console.log();" or by generating HTML.
+
+    POINTS TO CONSIDER:
+    1) We would like to see your thought process so feel free to speak out allowed / provide explanations in the form of inline comments.
+    2) If you do not have enough time to complete the 
+*/
+
+const wordList = ['yourselves', 'yesteryear', 'yellowtail', 'yellowwood', 'yellowhammer', 'yardmaster', 'youngberry', 'youngling',
+    'ytterbium', 'yellowware', 'yachtsman', 'yellowlegs', 'yellowfins', 'yellowthroat', 'yeastiness', 'yestereves', 'yesterdays',
+    'yeastinesses', 'yeomanries', 'yearningly', 'yardsticks', 'youngberries', 'younglings', 'youngsters', 'yottabytes', 'youngster',
+    'yesternights', 'youthquake', 'youthening', 'youthfully', 'yohimbines', 'yoctoseconds', 'yokefellow', 'ytterbiums', 'yesterday',
+    'yuppifying', 'yuppiedoms', 'youthfulness', 'yokozunas', 'ytterbias', 'ytterbous', 'yodellers'
+]
+
+function run() {
+    let i = 0;
+    console.log(i);
+}
